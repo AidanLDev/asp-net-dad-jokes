343536633295b77c015e7a92a0a20e0a6a6553e9
AidanLDev <dev@aidanlowson.com>
2023-10-17 22:56:19 +0100

    Adding framework for dad jokes app

---
+++ dad-jokes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dad_jokes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
