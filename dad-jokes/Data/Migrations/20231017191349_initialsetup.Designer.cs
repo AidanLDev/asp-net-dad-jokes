2bb405d2541453edb2f79debda3d452535355e4d
AidanLDev <dev@aidanlowson.com>
2023-10-17 20:14:52 +0100

    Adding migration

--- dad-jokes/Data/Migrations/20231017191349_initialsetup.Designer.cs
+++ dad-jokes/Data/Migrations/20231017191349_initialsetup.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using dad_jokes.Data;
 
@@ -11,9 +12,11 @@ using dad_jokes.Data;
 namespace dad_jokes.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231017191349_initialsetup")]
+    partial class initialsetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
