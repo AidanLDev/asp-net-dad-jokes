395516a03f75fb4db0aff9542084a70669afc09f
AidanLDev <dev@aidanlowson.com>
2023-10-23 09:19:31 +0100

    Getting the app started with SQL server

---
+++ dad-jokes/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using dad_jokes.Models;
+
+namespace dad_jokes.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<dad_jokes.Models.Joke> Joke { get; set; } = default!;
+    }
+}
